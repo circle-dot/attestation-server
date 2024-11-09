6816bc6cfffa8cbaa0b2b71dc6c27ee97859c8be
MarazzaM <marazza.matias@gmail.com>
2024-11-09 19:43:20 +0700

    Update stampsHistory

--- src/attestation/attestation.service.ts
+++ src/attestation/attestation.service.ts
@@ -198,20 +198,91 @@ export class AttestationService {
     const { wallet, attestationUID, stampId } = data;
 
     const stampsHistory = [
-      { id: '1', title: 'Music Prophet', description: 'You voted for three songs that ended up in the top 10. You\'ve got the magic touch!', icon: '/StampIt.png', isLocked: true },
-      { id: '2', title: 'Passionate Proposer', description: 'You\'ve proposed 10+ songs that have entered the top 100. You bring the hits!', icon: '/StampIt.png', isLocked: true },
-      { id: '3', title: 'Midnight DJ', description: 'You proposed or voted on songs during the "dead hours" and kept the playlist alive.', icon: '/StampIt.png', isLocked: true },
-      { id: '4', title: 'Lone Listener', description: 'You voted for a song that no one else dared to. We see you.', icon: '/StampIt.png', isLocked: true },
-      { id: '5', title: 'Party Catalyst', description: 'Your song was the first to reach 10 votes.', icon: '/StampIt.png', isLocked: true },
-      { id: '6', title: 'Genre Guru', description: 'You\'ve voted for 10+ songs of the same genre. You\'re kind of an expert, aren\'t you?', icon: '/StampIt.png', isLocked: true },
-      { id: '7', title: 'Love at First Sight', description: 'You proposed a song.', icon: '/StampIt.png', isLocked: true },
-      { id: '8', title: 'Diamond Curator', description: 'A song you voted ended up in the top 10.', icon: '/StampIt.png', isLocked: true },
-      { id: '9', title: 'Gold Curator', description: 'A song you voted ended up in the top 50.', icon: '/StampIt.png', isLocked: true },
-      { id: '10', title: 'Silver Curator', description: 'A song you voted ended up in the top 100.', icon: '/StampIt.png', isLocked: true },
-      { id: '11', title: 'Diverse Taste', description: 'You liked several genres while proposing.', icon: '/StampIt.png', isLocked: true },
-      { id: '12', title: 'All In', description: 'You used ALL your vouches.', icon: '/StampIt.png', isLocked: true },
+      {
+        id: '1',
+        title: 'Love at First Sight',
+        description: 'You proposed a song.',
+        imageurl: 'https://stereo.stamp.network/stamps/loveatfirstsight.png'
+      },
+      {
+        id: '2',
+        title: 'Getting there',
+        description: 'You vouched for 5 songs',
+        imageurl: 'https://stereo.stamp.network/stamps.png'
+      },
+      {
+        id: '3',
+        title: 'Livin on a prayer',
+        description: 'You vouched for 10 songs.',
+        imageurl: 'https://stereo.stamp.network/stamps/livinonaprayer.png'
+      },
+      {
+        id: '4',
+        title: 'All In',
+        description: 'You used ALL your vouches.',
+        imageurl: 'https://stereo.stamp.network/stamps/allin.png'
+      },
+      {
+        id: '5',
+        title: 'Midnight DJ',
+        description: 'You proposed or voted on songs during the "dead hours" and kept the playlist alive.',
+        imageurl: 'https://stereo.stamp.network/stamps/midnightdj.png'
+      },
+      {
+        id: '6',
+        title: 'Music Prophet',
+        description: 'You voted for three songs that ended up in the top 10. You\'ve got the magic touch!',
+        imageurl: 'https://stereo.stamp.network/stamps/musicprophet.png'
+      },
+      {
+        id: '7',
+        title: 'Passionate Proposer',
+        description: 'You\'ve proposed 10+ songs that have entered the top 100. You bring the hits!',
+        imageurl: 'https://stereo.stamp.network/stamps/passionateproposer.png'
+      },
+      {
+        id: '8',
+        title: 'Lone Listener',
+        description: 'You voted for a song that no one else dared to. We see you.',
+        imageurl: 'https://stereo.stamp.network/stamps/lonelistener.png'
+      },
+      {
+        id: '9',
+        title: 'Party Catalyst',
+        description: 'Your song was the first to reach 10 votes.',
+        imageurl: 'https://stereo.stamp.network/stamps/partycatalyst.png'
+      },
+      {
+        id: '10',
+        title: 'Genre Guru',
+        description: 'You\'ve voted for 10+ songs of the same genre. You\'re kind of an expert, aren\'t you?',
+        imageurl: 'https://stereo.stamp.network/stamps/genregu.png'
+      },
+      {
+        id: '11',
+        title: 'Diamond Curator',
+        description: 'A song you voted ended up in the top 10.',
+        imageurl: 'https://stereo.stamp.network/stamps/diamondcurator.png'
+      },
+      {
+        id: '12',
+        title: 'Gold Curator',
+        description: 'A song you voted ended up in the top 50.',
+        imageurl: 'https://stereo.stamp.network/stamps/goldcurator.png'
+      },
+      {
+        id: '13',
+        title: 'Silver Curator',
+        description: 'A song you voted ended up in the top 100.',
+        imageurl: 'https://stereo.stamp.network/stamps/silvercurator.png'
+      },
+      {
+        id: '14',
+        title: 'DevCon Sea Atendee',
+        description: 'You attended the DevCon SEA event.',
+        imageurl: 'https://stereo.stamp.network/stamps/attendee.png'
+      },
     ];
-
     // Find the stamp details from stampsHistory
     const stampDetails = stampsHistory.find(stamp => stamp.id === stampId);
     console.log('[handleSafeAttestation] Found stamp details:', stampDetails);
@@ -243,7 +314,7 @@ export class AttestationService {
       { name: 'stampId', value: ethers.encodeBytes32String(stampId), type: 'bytes32' },
       { name: 'name', value: stampDetails.title, type: 'string' },
       { name: 'description', value: stampDetails.description, type: 'string' },
-      { name: 'imageUrl', value: stampDetails.icon, type: 'string' },
+      { name: 'imageUrl', value: stampDetails.imageurl, type: 'string' },
     ]);
 
     try {
